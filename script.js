cbf647188f2124375e37f5a7443f1a922f6a442d
meri <mariana_asavoaiei@outlook.com>
2023-01-24 17:03:34 +0000

    created the first list item for the container div that holds the time blocks.

--- script.js
+++ script.js
@@ -3,3 +3,9 @@
 let currentDate = $("#currentDay");
 let today = moment();
 $("#currentDay").text("Today's date is: " + today.format('YYYY-MM-DD'));
+
+// We need to grab the div with the #container and inside it, create the list items, set their text and append them.
+let containerEl = $(".container");
+let timeBlock = $("<li>");
+timeBlock.text("9 AM");
+containerEl.append(timeBlock);
